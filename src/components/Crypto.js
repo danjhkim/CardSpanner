82c408d45c9785dc7485280c510df75253dfbe69
danjhkim <danjhkim@gmail.com>
2023-02-26 16:28:46 -0500

    table card swap

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Grid, Card, CardContent, Typography, Button } from '@mui/material';
+import {
+	Grid,
+	Card,
+	CardContent,
+	Typography,
+	Button,
+	Table,
+	TableHead,
+	TableRow,
+	TableCell,
+	TableBody,
+	TableContainer,
+	Paper,
+} from '@mui/material';
 import '../styles/Crypto.css';
 
 //temp API
@@ -8,6 +21,7 @@ const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 const Crypto = () => {
 	const [posts, setPosts] = useState([]);
 	const [startIndex, setStartIndex] = useState(0);
+	const [displayMode, setDisplayMode] = useState('card');
 
 	const handleNext = () => {
 		setStartIndex(startIndex + 4);
@@ -15,6 +29,10 @@ const Crypto = () => {
 
 	const handlePrev = () => {
 		setStartIndex(Math.max(0, startIndex - 4));
+	};
+
+	const handleToggleDisplayMode = () => {
+		setDisplayMode(displayMode === 'card' ? 'table' : 'card');
 	};
 
 	const visiblePosts = posts.slice(startIndex, startIndex + 4);
@@ -28,26 +46,50 @@ const Crypto = () => {
 	return (
 		<div className='cryptoBlock'>
 			<Grid container spacing={2}>
-				{visiblePosts.map(post => (
-					<Grid item xs={12} md={3} key={post.id}>
-						<Card>
-							<CardContent>
-								<Typography variant='h5' sx={{ mb: 4 }}>
-									{post.title}
-								</Typography>
-								<Typography variant='body1' sx={{ mb: 1 }}>
-									Body: {post.body}
-								</Typography>
-								<Typography variant='body1' sx={{ mb: 1 }}>
-									Body: {post.body}
-								</Typography>
-								<Typography variant='body1' sx={{ mb: 1 }}>
-									Body: {post.body}
-								</Typography>
-							</CardContent>
-						</Card>
+				{displayMode === 'card' ? (
+					visiblePosts.map(post => (
+						<Grid item xs={12} md={3} key={post.id}>
+							<Card>
+								<CardContent>
+									<Typography variant='h5' sx={{ mb: 4 }}>
+										{post.title}
+									</Typography>
+									<Typography variant='body1' sx={{ mb: 1 }}>
+										Body: {post.body}
+									</Typography>
+									<Typography variant='body1' sx={{ mb: 1 }}>
+										Body: {post.body}
+									</Typography>
+									<Typography variant='body1' sx={{ mb: 1 }}>
+										Body: {post.body}
+									</Typography>
+								</CardContent>
+							</Card>
+						</Grid>
+					))
+				) : (
+					<Grid item xs={12}>
+						<TableContainer component={Paper}>
+							<Table>
+								<TableHead>
+									<TableRow>
+										<TableCell>Title</TableCell>
+										<TableCell>Body</TableCell>
+									</TableRow>
+								</TableHead>
+								<TableBody>
+									{visiblePosts.map(post => (
+										<TableRow key={post.id}>
+											<TableCell>{post.title}</TableCell>
+											<TableCell>{post.body}</TableCell>
+										</TableRow>
+									))}
+								</TableBody>
+							</Table>
+						</TableContainer>
 					</Grid>
-				))}
+				)}
+
 				<Grid item xs={12}>
 					<Button
 						sx={{ mr: 4 }}
@@ -63,6 +105,14 @@ const Crypto = () => {
 						onClick={handleNext}
 						disabled={startIndex + 4 >= posts.length}>
 						Next
+					</Button>
+				</Grid>
+				<Grid item xs={12}>
+					<Button
+						variant='contained'
+						color='primary'
+						onClick={handleToggleDisplayMode}>
+						{displayMode === 'card' ? 'Show Table' : 'Show Card'}
 					</Button>
 				</Grid>
 			</Grid>
