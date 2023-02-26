4e1773470acd59d4297dff1a3084351e74a6ac6e
danjhkim <danjhkim@gmail.com>
2023-02-26 16:18:25 -0500

    added styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import Crypto from './components/Crypto';
 
 function App() {
--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Grid, Card, CardContent, Typography } from '@mui/material';
+import '../styles/Crypto.css';
 
 //temp API
 const API_URL = 'https://jsonplaceholder.typicode.com/posts';
@@ -7,6 +8,8 @@ const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 const Crypto = () => {
 	const [posts, setPosts] = useState([]);
 	const [startIndex, setStartIndex] = useState(0);
+
+	console.log(posts);
 
 	const visiblePosts = posts.slice(startIndex, startIndex + 4);
 
@@ -17,17 +20,23 @@ const Crypto = () => {
 	}, []);
 
 	return (
-		<div>
+		<div className='cryptoBlock'>
 			<Grid container spacing={2}>
 				{visiblePosts.map(post => (
 					<Grid item xs={12} md={3} key={post.id}>
 						<Card>
 							<CardContent>
-								<Typography variant='h5'>
+								<Typography variant='h5' sx={{ mb: 4 }}>
 									{post.title}
 								</Typography>
-								<Typography variant='body2'>
-									{post.body}
+								<Typography variant='body1' sx={{ mb: 1 }}>
+									Body: {post.body}
+								</Typography>
+								<Typography variant='body1' sx={{ mb: 1 }}>
+									Body: {post.body}
+								</Typography>
+								<Typography variant='body1' sx={{ mb: 1 }}>
+									Body: {post.body}
 								</Typography>
 							</CardContent>
 						</Card>
