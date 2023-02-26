5bb2f25a658c7f8a9f594569455ec7a1c26849de
danjhkim <danjhkim@gmail.com>
2023-02-26 15:54:42 -0500

    4 card show

--- src/components/Crypto.js
+++ src/components/Crypto.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { Grid, Card, CardContent, Typography } from '@mui/material';
 
 //temp API
 const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 const Crypto = () => {
 	const [posts, setPosts] = useState([]);
+	const [startIndex, setStartIndex] = useState(0);
+
+	const visiblePosts = posts.slice(startIndex, startIndex + 4);
 
 	useEffect(() => {
 		fetch(API_URL)
@@ -12,7 +16,26 @@ const Crypto = () => {
 			.then(data => setPosts(data));
 	}, []);
 
-	return <div>Crypto</div>;
+	return (
+		<div>
+			<Grid container spacing={2}>
+				{visiblePosts.map(post => (
+					<Grid item xs={12} md={3} key={post.id}>
+						<Card>
+							<CardContent>
+								<Typography variant='h5'>
+									{post.title}
+								</Typography>
+								<Typography variant='body2'>
+									{post.body}
+								</Typography>
+							</CardContent>
+						</Card>
+					</Grid>
+				))}
+			</Grid>
+		</div>
+	);
 };
 
 export default Crypto;
