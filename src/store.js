789bc5d8c5e5b2fc4223e9633f584f91f43d8ed7
danjhkim <danjhkim@gmail.com>
2023-02-27 13:19:02 -0500

    Updates

--- src/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import coinReducer from './slices/coinList';
-// import naming convention is kind of weird emeryjs does it?
-
-export default configureStore({
-	reducer: { coinReducer },
-});
